b2a224a60f5dca97c87e94fdbce8d942a43818e6
dianramdhani <dian.ramdhani@gmail.com>
2021-09-11 02:27:22 +0700

    update LetterResult: add copy

--- src/pages/LetterResult.vue
+++ src/pages/LetterResult.vue
@@ -8,12 +8,15 @@
       v-model="letter"
     ></textarea>
   </div>
-  <div class="alert alert-primary" role="alert">Copy success!</div>
-  <button class="btn btn-primary mb-1">Copy</button>
+  <div class="alert alert-primary" role="alert" v-if="copySuccess">
+    Copy success!
+  </div>
+  <button class="btn btn-primary mb-1" @click="copy">Copy</button>
   <router-link class="btn btn-secondary" to="/">Back</router-link>
 </template>
 
 <script>
+import { copyText } from "vue3-clipboard";
 import {} from "../store";
 import { GET_LETTER, TEMPLATE_IS_VALID } from "../store/getter-types";
 
@@ -22,12 +25,21 @@ export default {
   data() {
     return {
       letter: "",
+      copySuccess: false,
     };
   },
   mounted() {
     if (this.$store.getters[TEMPLATE_IS_VALID]) {
       this.letter = this.$store.getters[GET_LETTER];
     }
+  },
+  methods: {
+    copy() {
+      copyText(this.letter, undefined, (error) => {
+        this.copySuccess = !error;
+        setTimeout(() => (this.copySuccess = false), 3000);
+      });
+    },
   },
 };
 </script>
